4cc0eb82577e779ccf21ac36be7f93b5964bc6f8
Radpc <mgm3@icomp.ufam.edu.br>
2019-09-24 16:26:29 -0400

    added the --help part, and gitignore

--- main.cpp
+++ main.cpp
@@ -1,10 +1,33 @@
+#include <fstream>
 #include <iostream>
+#include <vector>
+
+#define HELP_FILE "help"
 
 using namespace std;
 
-int main(int argc, char *argv[]){
+int readHelp() {
+    ifstream reader;
+    string output;
 
+    reader.open(HELP_FILE);
 
-    
-    return 0.
+    if (reader.is_open()) {
+        while (getline(reader, output)) {
+            cout << output << endl;
+        }
+    }
+    reader.close();
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    vector<string> cmdLineArgs(argv, argv + argc);
+
+    for (auto &arg : cmdLineArgs) {
+        if (arg == "--help" || arg == "-help") {
+            readHelp();
+            return 0;
+        }
+    }
 }
