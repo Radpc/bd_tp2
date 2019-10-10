9efc3667c000c83d1e28e12336592780dbfcc9c7
Radpc <mgm3@icomp.ufam.edu.br>
2019-10-10 17:47:07 -0400

    Added header files for organization

---
+++ src/helper.cpp
@@ -0,0 +1,9 @@
+#include "helper.h"
+
+bool has_suffix(const std::string &str, const std::string &suffix) {
+    return str.size() >= suffix.size() &&
+           str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+}
+
+// Função para limpar a tela
+void clrscr() { system("@cls||clear"); }
---
+++ src/helper.h
@@ -0,0 +1,9 @@
+#ifndef HELPER_H
+#define HELPER_H
+#include <string>
+
+bool has_suffix(const std::string &str, const std::string &suffix);
+
+// Função para limpar a tela
+void clrscr();
+#endif
