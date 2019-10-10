9cb45ef43a99c35663f3d559de3375260979e2f0
Radpc <mgm3@icomp.ufam.edu.br>
2019-10-10 16:12:16 -0400

    Adicionado uma barra de loading

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,23 @@
+#include <pthread.h>  //Pthreads
+#include <unistd.h>
+#include <algorithm>  // std::count
 #include <fstream>
 #include <iostream>
+#include <string>
 #include <vector>
+
+#define SEC 1000000               // Valor de um segundo
+#define TAXA_ATUALIZACAO SEC / 2  // O fps
 
 #define HELP_FILE "help"
 #define MODE_INSERT 1
 
 using namespace std;
 
+// Global Variables
+double progress = 0;
+int lines = 0;
+int total_lines = 0;
 
 class csv_line {
    public:
@@ -34,6 +45,32 @@ class csv_line {
     string snippet;
 };
 
+// Função para limpar a tela
+void clrscr() { system("@cls||clear"); }
+
+void *t_progress(void *lpParam) {
+    while (progress <= 1.0) {
+        int barWidth = 70;
+
+        std::cout << "[";
+        int pos = barWidth * progress;
+        for (int i = 0; i < barWidth; ++i) {
+            if (i < pos)
+                std::cout << "=";
+            else if (i == pos)
+                std::cout << ">";
+            else
+                std::cout << " ";
+        }
+        std::cout << "] " << int(progress * 100.0) << " %\r";
+        std::cout.flush();
+
+        progress = lines / total_lines;
+        usleep(SEC);
+    }
+    std::cout << std::endl;
+}
+
 int readHelp() {
     ifstream reader;
     string output;
@@ -54,13 +91,17 @@ bool has_suffix(const string &str, const string &suffix) {
            str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
 }
 
-int insert_csv(string csv_name){
-    
-}
+int insert_csv(string csv_name) {}
 
 int main(int argc, char *argv[]) {
     vector<string> cmdLineArgs(argv, argv + argc);
     vector<string> insertFiles;
+    pthread_t aux;
+
+    // Limpar tela
+    clrscr();
+
+    // Iniciar modo
     int mode = -1;
 
     for (auto &arg : cmdLineArgs) {
@@ -72,6 +113,19 @@ int main(int argc, char *argv[]) {
             insertFiles.push_back(arg);
         } else if (arg == "-i" || arg == "--insert") {
             mode = MODE_INSERT;
+        }
+    }
+
+    for (auto i : insertFiles) {
+        ifstream my_file(i);
+        string line;
+        lines = 0;
+        if (my_file.is_open()) {
+            // Iniciar barra com progress
+            pthread_create(&aux, NULL, t_progress, NULL);
+
+            while (getline(my_file, line)) lines++;
+            cout << lines << endl;
         }
     }
 }
