e1f9e81bca78afe0f10fbbfc43923b29b0ef04e6
Radpc <mgm3@icomp.ufam.edu.br>
2019-10-13 19:27:00 -0400

    Added sequential search, Reduced mode and Timed mode. Fixed a few things also

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include <bits/stdc++.h>
 #include <pthread.h>  //Pthreads
 #include <unistd.h>   //Time for debugging
+#include <ctime>      //Time
 #include <fstream>    //Files
 #include <iostream>   //Files
 #include <regex>      //Get csv fields
@@ -25,10 +26,12 @@
 // Options define's
 #define SEC 1000000                // Valor de um segundo
 #define TAXA_ATUALIZACAO SEC / 10  // O fps
+#define REDUCE_TO 60
 
 // Config define's
 #define MODE_INSERT 1  // Codigo do modo de inserir
 #define MODE_FIND 2
+#define MODE_NORMAL 3
 
 // Retirar a necessidade de escrever std
 using namespace std;
@@ -36,6 +39,7 @@ using namespace std;
 /// Global Variables
 // Quantidade de linhas lidas já
 int lines = 0;
+bool reduced = false;
 
 /// Regex
 std::smatch m;
@@ -146,7 +150,6 @@ string create_db_folder(string csv_name) {
     char cmdd[cmd.size() + 1];
     strcpy(cmdd, cmd.c_str());
     system(cmdd);
-    cout << cmdd << endl;
     return folder;
 }
 
@@ -188,16 +191,19 @@ string findHash(string folder, int id, int mod1, int mod2) {
 
     string idd = "\"" + to_string(id) + "\"";
 
-
     ifstream file(folder + "/hashing/" + to_string(subfolder) + "/" +
                   to_string(bucket) + ".b");
     file.seekg(0, ios::beg);
 
     if (file.is_open()) {
         while (getline(file, line)) {
-            if (line.substr(0,idd.size()).compare(idd) == 0) {
+            if (line.substr(0, idd.size()).compare(idd) == 0) {
                 file.close();
-                return line;
+                if (reduced and line.size() > REDUCE_TO) {
+                    return line.substr(0, REDUCE_TO) + "...";
+                } else {
+                    return line;
+                }
             }
         }
         return "Could not find " + to_string(id) + ".";
@@ -206,8 +212,31 @@ string findHash(string folder, int id, int mod1, int mod2) {
         file.close();
         return "File could not be open! - " + folder;
     }
+}
 
-    
+string normalSearch(string csv, int id) {
+    ifstream file(csv);
+    string line;
+    string idd = "\"" + to_string(id) + "\"";
+    file.seekg(0, ios::beg);
+
+    if (file.is_open()) {
+        while (getline(file, line)) {
+            if (line.substr(0, idd.size()).compare(idd) == 0) {
+                file.close();
+                if (reduced and line.size() > REDUCE_TO) {
+                    return line.substr(0, REDUCE_TO) + "...";
+                } else {
+                    return line;
+                }
+            }
+        }
+        return "Could not find " + to_string(id) + ".";
+        file.close();
+    } else {
+        file.close();
+        return "CSV could not be open! - " + csv;
+    }
 }
 
 //##################################################\\============================================================================
@@ -218,20 +247,33 @@ string findHash(string folder, int id, int mod1, int mod2) {
 //##################################################\\============================================================================
 
 int main(int argc, char *argv[]) {
-    // Iniciar variáveis
+    // List of files
     vector<string> cmdLineArgs(argv, argv + argc);
     vector<string> insertFiles;
+
+    // Search flags
     int find = -1;
     string findPlace = "";
+    int normalFind = -1;
+    string normalPlace = "";
+
+    // Thread
     pthread_t progress;
+
+    // Global vars
     string folder;
     string line;
+
+    // Time measurements
+    bool timed = false;
+    clock_t time_start;
+    clock_t time_finish;
+    double total_time;
 
     // Iniciar modo
     int mode = -1;
 
     // Tratar os argumentos
-
     for (int i = 1; i < cmdLineArgs.size(); i++) {
         if (cmdLineArgs[i] == "--help" || cmdLineArgs[i] == "-h") {
             readHelp();
@@ -272,18 +314,42 @@ int main(int argc, char *argv[]) {
                 return -1;
             }
 
+        } else if (cmdLineArgs[i] == "--normal" ||
+                   cmdLineArgs[i] == "-n") {  // NORMAL FIND
+            mode = MODE_NORMAL;
+            i++;
+            if (i >= cmdLineArgs.size()) {
+                cout << "Argument number error!" << endl;
+                return -1;
+            }
+            normalPlace = cmdLineArgs[i];
+            i++;
+            if (i >= cmdLineArgs.size()) {
+                cout << "Argument number error!" << endl;
+                return -1;
+            }
+
+            if (isNumber(cmdLineArgs[i])) {
+                normalFind = stoi(cmdLineArgs[i]);
+            } else {
+                cout << "Error! Insert a valid ID" << endl;
+                return -1;
+            }
         } else if (mode == MODE_INSERT and
                    has_suffix(cmdLineArgs[i], ".csv")) {  // INSERT MULTIPLE
             insertFiles.push_back(cmdLineArgs[i]);
+        } else if (cmdLineArgs[i] == "-t" || cmdLineArgs[i] == "--timed") {
+            timed = true;
+        } else if (cmdLineArgs[i] == "-r" || cmdLineArgs[i] == "--reduced") {
+            reduced = true;
         } else {
-            cout << "Error" << endl;
-            ;
+            cout << "Invalid sintax! Maybe missed an \"-i\" ?" << endl;
             return -1;
         }
     }
 
     // Limpar tela
-    clrscr();
+    // clrscr();
 
     // Tratar as criações de bancos
     for (auto i : insertFiles) {
@@ -293,7 +359,7 @@ int main(int argc, char *argv[]) {
         lines = 0;
 
         if (my_file.is_open()) {
-            cout << "File open!" << endl;
+            cout << "File open!" << endl << endl;
 
             folder = create_db_folder(i);
 
@@ -302,11 +368,9 @@ int main(int argc, char *argv[]) {
             pthread_create(&progress, NULL, t_progress, NULL);
             while (getline(my_file, line)) lines++;
             progress = false;
-
             cout << "Reading - DONE - [" << lines << " linhas]" << endl;
-            cout.flush();
             cout << endl;
-
+            cout.flush();
             my_file.clear();
             my_file.seekg(0, ios::beg);
             lines = 0;
@@ -315,10 +379,11 @@ int main(int argc, char *argv[]) {
             cout << "Inserting in hash.." << endl;
             pthread_create(&progress, NULL, t_progress, NULL);
             while (getline(my_file, line)) {
-                insertHash(folder, line, 5, 7);
+                insertHash(folder, line, 23, 97);
                 lines++;
             }
             cout << "Hashing - DONE - [" << lines << " linhas]" << endl;
+            cout << endl;
             progress = false;
 
         } else {
@@ -327,11 +392,38 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // Search hash
     if (findPlace != "") {
         if (find != -1) {
-            cout << findHash(findPlace, find, 5, 7) << endl;
+            cout << "[Hash search]" << endl;
+            if (timed) time_start = clock();
+            cout << findHash(findPlace, find, 23, 97) << endl;
+            if (timed) {
+                time_finish = clock();
+                total_time = double(time_finish - time_start) / CLOCKS_PER_SEC;
+                cout << " Time elapsed: " << total_time << ";" << endl;
+            }
+            cout << "  ------" << endl;
         } else {
             cout << "Specify the path!" << endl;
+            return -1;
+        }
+    }
+
+    // Search normal
+    if (normalPlace != "") {
+        if (normalFind != -1) {
+            cout << "[Normal search]" << endl;
+            if (timed) time_start = clock();
+            cout << normalSearch(normalPlace, normalFind) << endl;
+            if (timed) {
+                time_finish = clock();
+                total_time = double(time_finish - time_start) / CLOCKS_PER_SEC;
+                cout << " Time elapsed: " << total_time << ";" << endl;
+            }
+            cout << "  ------" << endl;
+        } else {
+            cout << "Specify the csv!" << endl;
             return -1;
         }
     }
